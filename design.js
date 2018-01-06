bbb7f14520755fee976571d124d677d9f39325b8
Radostina Dimitrova <33595666+cristalnaya@users.noreply.github.com>
2018-01-06 14:27:48 +0200

    Add files via upload

--- design.js
+++ design.js
@@ -1,4 +1,12 @@
-// Skills Progress Bar
+$(document).ready(function() {
+	$("#menu").click(function() {
+		$("nav.nav_closed").toggleClass("nav_open");
+	});
+	$("nav ul li a").click(function() {
+		$("nav.nav_open").removeClass("nav_open");
+		});
+	});
+
 (function(document) {
 	var _bars = [].slice.call(document.querySelectorAll('.bar-inner'));
 _bars.map(function(bar, index) {
@@ -7,3 +15,26 @@ _bars.map(function(bar, index) {
 		}, index * 1000);
 	})
 })(document);
+
+$(document).ready(function() {
+	var btt = $('.back-to-top');
+	btt.on('click', function(e) {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+		e.preventDefault();
+	});
+	$(window).on('scroll', function() {
+		var self =$(this),
+			height = self.height(),
+			top = self.scrollTop();
+
+		if (top > height) {
+				if (!btt.is(':visible')) {
+						btt.show();
+				}
+		} else {
+				btt.hide();
+		}
+	});
+});
